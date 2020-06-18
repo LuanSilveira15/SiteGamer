882145b43a7f2407319c161355562c32beed0628
Luan Silveira <64423070+LuanSilveira15@users.noreply.github.com>
2020-06-18 16:22:33 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const titulo = document.querySelector('.dados');
+    typeWriter(titulo);
+    function typeWriter(elemento) {
+      const textoArray = elemento.innerHTML.split('');
+      elemento.innerHTML = '';
+      textoArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 200 * i);
+      });
+    }
+
+
